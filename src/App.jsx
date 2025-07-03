abc52cded7451b0b22fad3b7083752fb44350f64
jooky <jooky0327@naver.com>
2025-07-03 17:34:26 +0900

    added function

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,72 @@
 import { useEffect, useState } from 'react'
-
 function App() {
-    useEffect(() => {
-        fetch('https://dummyjson.com/todos')
-            .then((res) => res.json())
-            .then((res) => setTodos(res.todos))
-    }, [])
-
     const [todoId, setTodoId] = useState(4)
+    const [todos, setTodos] = useState([
+        { id: 1, text: '공부하기', checked: false },
+        { id: 2, text: '밥먹고 커피먹기', checked: false },
+        { id: 3, text: '헬스장 갔다 샤워하기', checked: false },
+    ])
 
-    const [todos, setTodos] = useState([])
-
+    //입력 폼 처리
     const handleOnSubmit = (e) => {
         e.preventDefault()
-        const form = e.target
-
-        fetch('https://dummyjson.com/todos/add', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                todo: form.todo.value,
-                completed: false,
-                userId: 5,
-            }),
-        })
-            .then((res) => res.json())
-            .then(console.log)
-
-        // setTodos([{ id: todoId, todo: form.todo.value, completed: false }, ...todos])
-        // setTodoId(todoId + 1)
+        const form = { id: todoId, text: e.target.elements.todo.value, checked: false }
+        setTodoId(todoId + 1)
+        setTodos([form, ...todos])
     }
 
-    const removeTodo = (seletedId) => {
-        const filterTodos = todos.filter((todo) => todo.id != seletedId)
-        setTodos(filterTodos)
+    //삭제
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => id !== todo.id))
     }
 
-    const toggleTodo = (seletedId) => {
-        const updateTodos = todos.map((todo) => (todo.id == seletedId ? { ...todo, completed: !todo.completed } : todo))
-        setTodos(updateTodos)
+    const toggleTodo = (id) => {
+        setTodos(todos.map((todo) => (id === todo.id ? { ...todo, checked: !todo.checked } : todo)))
+    }
+
+    // 우선순위 위로
+    const moveUp = (index) => {
+        if (index === 0) return
+        const newTodos = [...todos]
+        ;[newTodos[index - 1], newTodos[index]] = [newTodos[index], newTodos[index - 1]]
+        setTodos(newTodos)
+    }
+
+    // 우선순위 아래로
+    const moveDown = (index) => {
+        if (index === todos.length - 1) return
+        const newTodos = [...todos]
+        ;[newTodos[index + 1], newTodos[index]] = [newTodos[index], newTodos[index + 1]]
+        setTodos(newTodos)
     }
 
     return (
         <>
+            <h1>ToDo List</h1>
+
+            <ul>
+                {[...todos]
+                    .sort((a, b) => a.checked - b.checked)
+                    .map((todo, i) => (
+                        <li key={todo.id} style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                            <input type="checkbox" checked={todo.checked} onChange={() => toggleTodo(todo.id)} />
+                            {todo.text}
+                            <button onClick={() => removeTodo(todo.id)}>X</button>
+                            <button onClick={() => moveUp(i)}>↑</button>
+                            <button onClick={() => moveDown(i)}>↓</button>
+                        </li>
+                    ))}
+            </ul>
+            <h3>
+                전체 일 : {todos.length} <br></br>
+                완료한 일 : {todos.filter((todo) => todo.checked).length} <br></br>
+                <span style={{ color: 'red' }}>처리할 일 : {todos.filter((todo) => !todo.checked).length}</span>
+            </h3>
+
             <form onSubmit={handleOnSubmit}>
                 <input type="text" name="todo" />
                 <button type="submit">등록</button>
             </form>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        <input
-                            type="checkbox"
-                            onChange={() => {
-                                toggleTodo(todo.id)
-                            }}
-                            checked={todo.completed}
-                        />
-                        {JSON.stringify(todo.completed)} / {todo.id} / {todo.todo}
-                        <button onClick={() => removeTodo(todo.id)}>X</button>
-                    </li>
-                ))}
-            </ul>
         </>
     )
 }
